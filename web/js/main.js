c96b62f681ea33296cf5285d539ac6f6cb2623a4
Adam Fister <arfister@gmail.com>
2019-03-04 15:27:00 -0500

    add separate js pagef

---
+++ web/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+$(document).ready(function () {
+    $('[data-toggle="popover"]').popover();
+});
+
+$(window).on("scroll", function () {
+    var scrollPos = $(window).scrollTop();
+    $('.navbar-default').addClass('top-of-page');
+
+});
+
+$('.navbar-nav>li>a').on('click', function () {
+    $('.navbar-collapse').collapse('hide');
+});
+
+$(document).ready(function(){
+$('.navbar-nav .nav-item').click(function(){
+$('.navbar-nav .nav-item').removeClass('active');
+$(this).addClass('active');
+})
+});
